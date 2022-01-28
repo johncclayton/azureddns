b8e7b0edbca8febeab1b4c48204dbd3ab8ca42ed
John Clayton <johnclayton72@gmail.com>
2022-01-28 13:53:05 +0100

    The build should still occur in the root of the source.

--- terraform/build.ps1
+++ terraform/build.ps1
@@ -3,6 +3,6 @@ $exclude = @(".vs", "deploy", "build.ps1", ".gitignore", ".terra*", "*.tf", ".vs
 New-Item -ItemType Directory -Force -Path deploy
 
 Push-Location .. 
-& dotnet publish --sc -c Release -o terraform/build
+& dotnet publish --sc -c Release -o build
 Get-ChildItem -Path ./build -Exclude $exclude | Compress-Archive -DestinationPath terraform/deploy/functionapp.zip -Force
 Pop-Location
