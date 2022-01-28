12e1d8651ff32986d03e65dc91c0b248a36a870a
John Clayton <johnclayton72@gmail.com>
2022-01-28 11:02:45 +0100

    Fix up client id setting on providers file.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   features {}
 
-  client_id         = var.spn-secret-id
+  client_id         = var.spn-client-id
   client_secret     = var.spn-secret-value
   tenant_id         = var.spn-tenant-id
 }
