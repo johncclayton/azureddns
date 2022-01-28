5b20aeaa687dc8d56319f65d72310c50a8346bc3
John Clayton <johnclayton72@gmail.com>
2022-01-28 10:58:43 +0100

    Fix build.

--- terraform/build.ps1
+++ terraform/build.ps1
@@ -1,4 +1,6 @@
 $exclude = @(".vs", "deploy", "build.ps1", ".gitignore", ".terra*", "*.tf", ".vscode")
+Push-Location .. 
 New-Item -ItemType Directory -Force -Path deploy
 & dotnet publish --sc -c Release -o build
 Get-ChildItem -Path ./build -Exclude $exclude | Compress-Archive -DestinationPath deploy/functionapp.zip -Force
+Pop-Location
